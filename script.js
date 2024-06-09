2e9b4aa9afa827a1787508f8b1cfc3bc1e4d9fce
mkrtichg <mkrtichgevorgian@gmail.com>
2024-06-09 13:38:01 +0400

    several changes
    added previous and next picture-buttons, background colors, removed borders and etc.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const contentPreview = document.getElementById("contentPreview")
 const ul = document.getElementById("content");
 const picture = document.getElementById("picture");
 const description = document.getElementById("description");
+
+const imgL = document.createElement('img');
+const imgR = document.createElement('img');
+
 // URLs
 const urlByDepartments = "https://collectionapi.metmuseum.org/public/collection/v1/departments";
 const objectsUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects";
@@ -17,7 +21,7 @@ let objectIDs;
 
 
 async function getObjectById(imgUrlById) {
-    debugger;
+    // debugger;
 
     try {
         const response = await fetch(imgUrlById);
@@ -25,37 +29,110 @@ async function getObjectById(imgUrlById) {
 
         const imgUrl = data.primaryImage;
         const img = document.createElement('img');
+
         img.src = imgUrl;
         img.alt = data.title;
-        img.id = "img";
+        img.id = "mainViewImage";
+        imgL.src = "./images/slide-left.png";
+        imgL.alt = "LeftArrow";
+        imgL.id = "imgLeftArrow";
+        imgR.src = "./images/slide-right.png";
+        imgR.alt = "RightdArrow";
+        imgR.id = "imgRightArrow";
         const sourceLink = data.objectURL;
         const source = document.createElement('a');
         source.href = sourceLink;
-        source.target="_blank"
+        source.target = "_blank"
         picture.innerHTML = "";
         description.innerHTML = "";
         source.append(img)
+        picture.append(imgL);
         picture.append(source);
+        picture.append(imgR);
+        const titleP = document.createElement('p');
+        const titleS = document.createElement('strong');
+        titleP.className = 'descCont';
+        const artistP = document.createElement('p');
+        const artistS = document.createElement('strong');
+        artistP.id = 'descCont';
+        const departmentP = document.createElement('p');
+        const departmentS = document.createElement('strong');
+        departmentP.id = 'descCont';
+        const dateP = document.createElement('p');
+        const dateS = document.createElement('strong');
+        dateP.id = 'descCont';
+        const cultureP = document.createElement('p');
+        const cultureS = document.createElement('strong');
+        cultureP.id = 'descCont';
+        const mediumP = document.createElement('p');
+        const mediumS = document.createElement('strong');
+        mediumP.id = 'descCont';
+        const dimensionsP = document.createElement('p');
+        const dimensionsS = document.createElement('strong');
+        dimensionsP.id = 'descCont';
+        const classificationP = document.createElement('p');
+        const classificationS = document.createElement('strong');
+        classificationP.id = 'descCont';
+        const repositoryP = document.createElement('p');
+        const repositoryS = document.createElement('strong');
+        repositoryP.id = 'descCont';
+
         const span = document.createElement('span');
-        span.id = 'descText';
-        span.innerText = "",
+        
+        const strong = document.createElement('strong');
+        span.id = 'desctiptionText';
+        titleP.innerHTML = "";
+        artistP.innerHTML = "";
+        departmentP.innerHTML = "";
+        dateP.innerHTML = "";
+        cultureP.innerHTML = "";
+        mediumP.innerHTML = "";
+        dimensionsP.innerHTML = "";
+        classificationP.innerHTML = "";
+        repositoryP.innerHTML = "";
+        titleS.innerText = "Title:";
+        artistS.innerText = "Artist:";
+        departmentS.innerText = "Department:";
+        dateS.innerText = "Date:";
+        cultureS.innerText = "Culture:";
+        mediumS.innerText = "Medium:";
+        dimensionsS.innerText = "Dimensions:";
+        classificationS.innerText = "Classification:";
+        repositoryS.innerText = "Repository:";
+        titleP.append(titleS, ` ${data.title}`)
+        artistP.append(artistS, ` ${data.artistDisplayName} (${data.artistDisplayBio})`)
+        departmentP.append(departmentS, ` ${data.department}`);
+        dateP.append(dateS, ` ${data.objectDate}`);
+        cultureP.append(cultureS, ` ${data.culture}`);
+        mediumP.append(mediumS, ` ${data.medium}`);
+        dimensionsP.append(dimensionsS, ` ${data.dimensions}`);
+        classificationP.append(classificationS, ` ${data.classification}`);
+        repositoryP.append(repositoryS, ` ${data.repository}`);
+
+        span.innerText = "";
+        // paragraph.append(
+
+        // )
             span.innerText = `        
-         Title: ${data.title}
-         Artist: ${data.artistDisplayName} (${data.artistDisplayBio})
-         Department: ${data.department}
-         Date: ${data.objectDate}
-         Culture: ${data.culture}
-         Medium: ${data.medium}
-         Dimensions: ${data.dimensions}
-         Classification: ${data.classification}
-         Repository: ${data.repository}
+       
+
+
+
+
+
+
+
+
+
+
+
          
          
          
          To view the next or previous picture please press the RIGHT or Left arrow on the keyboard.
          `;
 
-        description.append(span);
+        description.append(titleP,artistP, departmentP, dateP, cultureP, mediumP, dimensionsP, classificationP, repositoryP, span);
 
     } catch (error) {
         console.log(error.message);
@@ -86,7 +163,7 @@ async function previewImage(url) {
 
 async function createPreviewImages(params = []) {
     // debugger;    
-    contentPreview.innerHTML = "";   
+    contentPreview.innerHTML = "";
 
     for (let i = 0; i < params.length; i++) {
         await previewImage(params[i])
@@ -98,7 +175,7 @@ async function createPreviewImages(params = []) {
 async function getArrayOfObjectIds(urlByDep) {
     // debugger;
 
-    try {        
+    try {
         const response = await fetch(urlByDep);
         const data = await response.json();
         // data.objectIDs.forEach(objectID => objectIDs.push(objectID));
@@ -120,6 +197,7 @@ async function getAndCreateListByDepartments(url) {
         const data = await response.json();
         const departments = data.departments;
         const img = document.createElement('img');
+        img.id = "mainViewImage";
         img.src = 'https://cdn.sanity.io/images/cctd4ker/production/b8a5e07c166342e1c7f6fe30b8c45d64fea69ea5-4096x2326.jpg?w=3840&q=75&fit=clip&auto=format';
         picture.append(img);
         contentPreview.innerHTML = "";
@@ -146,14 +224,14 @@ async function getAndCreateListByDepartments(url) {
                 // debugger;
                 i = 0;
                 // objectIDs.splice(0);
-             
+
 
                 getArrayOfObjectIds(urlByDepartmentId(id));
 
                 getObjectById(urlByObjectId(objectIDs[i]));
                 createPreviewImages([urlByObjectId(objectIDs[i + 1]), urlByObjectId(objectIDs[i + 2]), urlByObjectId(objectIDs[i + 3])]);
 
-             
+
 
 
 
@@ -165,29 +243,61 @@ async function getAndCreateListByDepartments(url) {
     } catch (error) {
         console.log(error.message);
     }
+
+
+    document.addEventListener('keydown', (event) => {
+        // debugger;
+
+
+        switch (event.code) {
+            case 'ArrowLeft': {
+                if (i === 0) { break };
+                i--;
+                break;
+            }
+            case 'ArrowRight': {
+                if (i === objectIDs.length - 1) { break };
+                i++;
+                break;
+            }
+        }
+
+        getObjectById(urlByObjectId(objectIDs[i]));
+        createPreviewImages([urlByObjectId(objectIDs[i + 1]), urlByObjectId(objectIDs[i + 2]), urlByObjectId(objectIDs[i + 3])]);
+
+    });
+
+    imgL.addEventListener('click', () => {
+        // debugger;
+
+        if (i === 0) {
+            return
+        } else {
+            i--;
+
+        }
+        getObjectById(urlByObjectId(objectIDs[i]));
+        createPreviewImages([urlByObjectId(objectIDs[i + 1]), urlByObjectId(objectIDs[i + 2]), urlByObjectId(objectIDs[i + 3])]);
+
+    });
+
+
+    imgR.addEventListener('click', () => {
+        // debugger;
+
+        if (i === objectIDs.length - 1) {
+            return
+        } else {
+            i++;
+        }
+        getObjectById(urlByObjectId(objectIDs[i]));
+        createPreviewImages([urlByObjectId(objectIDs[i + 1]), urlByObjectId(objectIDs[i + 2]), urlByObjectId(objectIDs[i + 3])]);
+
+    })
+
 }
 
-document.addEventListener('keydown', (event) => {
-    // debugger;
-    
 
-    switch (event.code) {
-        case 'ArrowLeft': {
-            if (i === 0) { break };
-            i--;
-            break;
-        }
-        case 'ArrowRight': {
-            if (i === objectIDs.length - 1) { break };
-            i++;
-            break;
-        }
-    }
-
-    getObjectById(urlByObjectId(objectIDs[i]));
-    createPreviewImages([urlByObjectId(objectIDs[i + 1]), urlByObjectId(objectIDs[i + 2]), urlByObjectId(objectIDs[i + 3])]);
-
-});
 
 getAndCreateListByDepartments(urlByDepartments);
 
